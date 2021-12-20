8136c8186a0623edb88e220e9eeb05bd396d5710
Luthan95 <luthan@opsmx.io>
2021-12-20 16:31:31 +0530

    rbac changes

---
+++ rbac/custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/CreateApplicationStage.java
@@ -0,0 +1,24 @@
+package com.opsmx.plugin.stage.custom;
+
+import javax.validation.constraints.NotNull;
+
+import org.pf4j.Extension;
+import org.springframework.stereotype.Component;
+
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder;
+import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode;
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
+
+import groovy.transform.CompileStatic;
+
+@Component
+@Extension
+@CompileStatic
+public class CreateApplicationStage implements StageDefinitionBuilder {
+	
+	@Override
+	public void taskGraph(@NotNull StageExecution stage, @NotNull TaskNode.Builder builder) {
+		builder.withTask("app", UpsertApplicationTask.class);
+	}
+
+}
