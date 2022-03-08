de44c00aace904baa99f875ae231119e9be3cda0
Lalit Vikram Singh <lvikramsingh@lvikram-ltm2rhe.internal.salesforce.com>
2022-03-08 09:06:17 +0530

    backend for terraform plan

---
+++ terraform-plan/custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/TerraformPlanPlugin.kt
@@ -0,0 +1,37 @@
+package com.opsmx.plugin.stage.custom
+
+import com.netflix.spinnaker.kork.plugins.api.PluginSdks
+import com.netflix.spinnaker.orca.api.preconfigured.jobs.PreconfiguredJobConfigurationProvider
+import com.netflix.spinnaker.orca.clouddriver.config.KubernetesPreconfiguredJobProperties
+import org.pf4j.Extension
+import org.pf4j.Plugin
+import org.pf4j.PluginWrapper
+
+
+class TerraformPlanPlugin(wrapper: PluginWrapper): Plugin(wrapper) {
+
+    override fun start() {
+        System.out.println("LitmuschaosPlugin.start()")
+    }
+
+    override fun stop() {
+        System.out.println("LitmuschaosPlugin.stop()")
+    }
+}
+
+@Extension
+class TerraformPlanPreconfiguredStage(val pluginSdks: PluginSdks, val configuration: PluginConfig) : PreconfiguredJobConfigurationProvider {
+    override fun getJobConfigurations(): List<KubernetesPreconfiguredJobProperties> {
+        val jobProperties = pluginSdks.yamlResourceLoader().loadResource("io/opsmx/plugin/preconfigured/TSPlanJob.yaml", KubernetesPreconfiguredJobProperties::class.java)
+        if (!configuration.account.isNullOrEmpty()) {
+            jobProperties.account = configuration.account
+        }
+        if (!configuration.namespace.isNullOrEmpty()) {
+            jobProperties.manifest.metadata.namespace = configuration.namespace
+        }
+        if (!configuration.application.isNullOrEmpty()) {
+            jobProperties.application = configuration.application
+        }
+        return arrayListOf(jobProperties)
+    }
+}
