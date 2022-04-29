a3bac3117e754da441e9d15870fcae10136ef938
sharathchandrahc <sharath.chandra@opsmx.io>
2022-04-29 12:34:57 +0530

    Gate Security Component Updated

--- gate-approval/custom-stage-deck/src/input/dynamicFormFields.tsx
+++ gate-approval/custom-stage-deck/src/input/dynamicFormFields.tsx
@@ -11,20 +11,27 @@ import {
   ValidationMessage,
   HelpContentsRegistry,
   HelpField,
+  ReactSelectInput
 } from '@spinnaker/core';
 import { FieldArray, FormikProvider } from 'formik';
+
 
 interface IEvaluateVariablesStageFormProps extends IFormikStageConfigInjectedProps {
   chosenStage: IStageForSpelPreview;
   headers: [];
   blockLabel: string;
   isMultiSupported: boolean;
+  selectInput: boolean;
+  options: [];
+  connectorsList: [];
+  accountsList: [];
+  handleOnSelection: any ;
   parentIndex: number;
 }
 export function EvaluateVariablesStageForm(props: IEvaluateVariablesStageFormProps) {
-  const { formik, headers, blockLabel, isMultiSupported, parentIndex } = props;
+  const { formik, headers, blockLabel, isMultiSupported, parentIndex, selectInput, connectorsList, accountsList,  handleOnSelection } = props;
   const stage = props.formik.values;
-  const variables = stage.parameters.connectors[parentIndex].values ?? [];
+  const variables: any = stage?.parameters?.payloadConstraint[parentIndex]?.values ? stage.parameters.payloadConstraint[parentIndex].values : [];
   const isMountedRef = useIsMountedRef();
   const emptyValue = (() => {
     const obj: any = {};
@@ -41,81 +48,185 @@ export function EvaluateVariablesStageForm(props: IEvaluateVariablesStageFormPro
       // The end effect is that the red validation warnings are shown immediately when the Evaluate Variables stage is added.
       setTimeout(
         () =>
-          isMountedRef.current && formik.setFieldValue(`parameters.connectors[${parentIndex}].values`, [emptyValue]),
+          isMountedRef.current && formik.setFieldValue(`parameters.payloadConstraint[${parentIndex}].values`, [emptyValue]),
         100,
       );
     }
   }, [variables]);
   const FieldLayoutComponent = React.useContext(LayoutContext);
   const [deleteCount, setDeleteCount] = React.useState(0);
+
+  
+
+
+
   return (
     <>
       <table>
         <thead>
           <tr>
+            {/* {JSON.stringify(specificParams)} */}
             {headers.map((header: any) => {
-              HelpContentsRegistry.register('approval' + blockLabel + header.name, header.helpText);
+              HelpContentsRegistry.register(blockLabel + header.name, header.helpText);
               return (
                 <th key={header.name}>
-                  {header.label} <HelpField id={'approval' + blockLabel + header.name} />
+                  {header.label} <HelpField id={blockLabel + header.name} />
                 </th>
               );
             })}
           </tr>
         </thead>
-        <FormikProvider value={formik}>
-          <FieldArray
-            key={deleteCount}
-            name={`parameters.connectors[${parentIndex}].values`}
-            render={(arrayHelpers) => (
-              <>
-                <FieldLayoutComponent input={null} validation={{ hidden: true } as any} />
-                {variables.map((_: any, index: number) => {
-                  const onDeleteClicked = () => {
-                    setDeleteCount((count) => count + 1);
-                    arrayHelpers.handleRemove(index)();
-                  };
-                  return (
-                    <tr key={`${deleteCount}-${index}`}>
-                      {headers.map((header: any) => (
-                        <td key={`${header.name}-td`}>
+        <tbody>
+          <tr>
+            <td>
+              {!selectInput ? 
+              
+              <FormikProvider value={formik}>
+            <FieldArray
+              key={deleteCount}
+              name={`parameters.payloadConstraint[${parentIndex}].values`}
+              render={(arrayHelpers) => (
+                
+                <>
+                  <FieldLayoutComponent input={null} validation={{ hidden: true } as any} />
+                  {variables.map((_: any, index: number) => {
+                    
+                    const onDeleteClicked = () => {
+                      setDeleteCount((count) => count + 1);
+                      arrayHelpers.handleRemove(index)();
+                    };
+                    return (
+                      <tr key={`${deleteCount}-${index}`}>
+                        {headers.map((header: any) => (
+                          <td key={`${header.name}-td`}>
+
                           <FormikFormField
-                            name={`parameters.connectors[${parentIndex}].values[${index}][${header.name}]`}
-                            input={(inputProps) => <TextInput {...inputProps} placeholder={` Enter ${header.label}`} />}
-                            layout={VariableNameFormLayout}
-                          />
-                        </td>
-                      ))}
-                      {isMultiSupported === true ? (
-                        <td className="deleteBtn">
-                          <Tooltip value="Remove row">
-                            <button className="btn btn-sm btn-default" onClick={onDeleteClicked}>
-                              <span className="glyphicon glyphicon-trash" />
-                            </button>
-                          </Tooltip>
-                        </td>
-                      ) : null}
-                    </tr>
-                  );
-                })}
-                <tr>
-                  {isMultiSupported ? (
-                    <td colSpan={headers.length + 1}>
-                      <button
-                        type="button"
-                        className="btn btn-block btn-sm add-new"
-                        onClick={arrayHelpers.handlePush(emptyValue)}
-                      >
-                        <span className="glyphicon glyphicon-plus-sign" />
-                        Add row
-                      </button>
-                    </td>
-                  ) : null}
-                </tr>
-              </>
-            )}
-          />
-        </FormikProvider>
+                            name={`parameters.payloadConstraint[${parentIndex}].values[${index}][${header.name}]`}
+                            input={(inputProps) => <TextInput {...inputProps} placeholder={` Enter ${header.label}`} />
+                          }
+                        layout={VariableNameFormLayout}
+                        />
+                          </td>
+                        ))}
+                        {isMultiSupported === true ? (
+                          <td className="deleteBtn">
+                            <Tooltip value="Remove row">
+                              <button className="btn btn-sm btn-default" onClick={onDeleteClicked}>
+                                <span className="glyphicon glyphicon-trash" />
+                              </button>
+                            </Tooltip>
+                          </td>
+                        ) : null}
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    {isMultiSupported ? (
+                      <td colSpan={headers.length + 1}>
+                        <button
+                          type="button"
+                          className="btn btn-block btn-sm add-new"
+                          onClick={arrayHelpers.handlePush(emptyValue)}
+                        >
+                          <span className="glyphicon glyphicon-plus-sign" />
+                          Add row
+                        </button>
+                      </td>
+                    ) : null}
+                  </tr>
+                </>
+              )}
+            />
+          </FormikProvider>
+              
+              
+              
+              : 
+              
+              
+              <FormikProvider value={formik}>
+            <FieldArray
+              key={deleteCount}
+              name={`parameters.payloadConstraint[${parentIndex}].values`}
+              render={(arrayHelpers) => (
+                
+                <>
+                  <FieldLayoutComponent input={null} validation={{ hidden: true } as any} />
+                  {variables.map((_: any, index: number) => {
+                    
+                    const onDeleteClicked = () => {
+                      setDeleteCount((count) => count + 1);
+                      arrayHelpers.handleRemove(index)();
+                    };
+                    return (
+                      <tr key={`${deleteCount}-${index}`}>
+                        {headers.map((header: any) => (
+                          <td key={`${header.name}-td`}>
+                            
+                            <FormikFormField
+                              name={`parameters.connectorDetails[${parentIndex}].values[${index}][${header.name}]`}
+                              input={(inputProps) => 
+                                <ReactSelectInput
+                                  {...inputProps}
+                                  clearable={false}
+                                  options={ header.label === 'Connector' ? connectorsList.map((e:any) => ({
+                                    value: e,
+                                    label: e
+                                  })) : accountsList.map((e:any) => ({
+                                    value: e,
+                                    label: e
+                                  }))}
+                                  // value={header.label === 'Connector' ? connectorValue : accountValue}
+                                  // value={...props}
+                                  onChange={(e)=> handleOnSelection(e, header.label)}
+                                  //stringOptions={...props}
+                                  
+                                  />
+                                  
+                            }
+                              layout={VariableNameFormLayout}
+                            />
+                          </td>
+                        ))}
+                        {isMultiSupported === true ? (
+                          <td className="deleteBtn">
+                            <Tooltip value="Remove row">
+                              <button className="btn btn-sm btn-default" onClick={onDeleteClicked}>
+                                <span className="glyphicon glyphicon-trash" />
+                              </button>
+                            </Tooltip>
+                          </td>
+                        ) : null}
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    {isMultiSupported ? (
+                      <td colSpan={headers.length + 1}>
+                        <button
+                          type="button"
+                          className="btn btn-block btn-sm add-new"
+                          onClick={arrayHelpers.handlePush(emptyValue)}
+                        >
+                          <span className="glyphicon glyphicon-plus-sign" />
+                          Add row
+                        </button>
+                      </td>
+                    ) : null}
+                  </tr>
+                </>
+              )}
+            />
+          </FormikProvider>
+              
+              
+              
+              }
+
+          
+            </td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
