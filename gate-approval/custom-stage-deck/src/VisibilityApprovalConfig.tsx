3f51e41b05b00a70ecb2be1f1a4c1d889901edd1
sharathchandrahc <sharath.chandra@opsmx.io>
2022-05-10 15:04:43 +0530

    Able to load empty form with Approval Groups

--- gate-approval/custom-stage-deck/src/VisibilityApprovalConfig.tsx
+++ gate-approval/custom-stage-deck/src/VisibilityApprovalConfig.tsx
@@ -27,6 +27,7 @@ import {
 } from '@spinnaker/core';
 import './VisibilityApproval.less';
 import { EvaluateVariablesStageForm } from './input/dynamicFormFields';
+import { MultiSelect } from 'react-multi-select-component';
 
 const HorizontalRule = () => (
   <div className="grid-span-4">
@@ -46,18 +47,123 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
     </div>
   );
 
+  console.log("Main Return", props);
 
-  // Environments
+
+  //Load Initial Values
+  const [parametersPresent, setParametersPresent] = useState(false);
+
+    useEffect(()=> {  
+    if(!props.stage.hasOwnProperty('parameters')){
+      console.log("First Call");
+      
+      props.stage.parameters = {}
+    }
+    
+    if(!props.stage.parameters.hasOwnProperty('environment')){
+      props.stage.parameters.environment = [{
+      "id": null,
+      "spinnakerEnvironment": ""
+    }]
+    }
+    
+    if(!props.stage.parameters.hasOwnProperty('approvalGroups')){
+      props.stage.parameters.approvalGroups = []
+    }
+
+
+
+    REST(`platformservice/v2/applications/name/${props.application['applicationName']}`).
+    get()
+    .then(
+      (application)=> {
+      props.stage.applicationId = JSON.parse(application.applicationId);
+
+        REST(`platformservice/v4/applications/${props.stage.applicationId}`).
+        get()
+        .then(
+          (results)=> {
+            getServicePipelineDetails(results);       
+          }     
+        )
+      }     
+    )
+    setParametersPresent(true);
+
+
+    console.log("Props: ", props);
+
+  }, []);
+
+
+  const getGateSecurityParams = () => {
+    if(!props.stage?.parameters.hasOwnProperty('gateSecurity')){
+      props.stage.parameters.gateSecurity = [
+      {
+        "connectorType": "PayloadConstraints",
+        "helpText": "Payload Constraints",
+        "isMultiSupported": true,
+        "label": "Payload Constraints",
+        "selectInput": false,
+        "supportedParams": [
+          {
+            "helpText": "Key",
+            "label": "Key",
+            "name": "label",
+            "type": "string"
+          },
+          {
+            "helpText": "Value",
+            "label": "Value",
+            "name": "value",
+            "type": "string"
+          }
+        ],
+        "values": [
+          {
+            "label": "",
+            "value": ""
+          }
+        ]
+      }
+    ]
+    }
+  }
+
+    // Environments
     const[environmentsList , setenvironmentsList] = useState([]);
     useEffect(()=> {  
+      console.log("Environment API Called");
+
     REST('oes/accountsConfig/spinnaker/environments').
     get()
     .then(
       (results)=> {
+        console.log("Environemnt results: ", results);
+        
         setenvironmentsList(results);       
       }     
     )
   }, []) 
+
+    //Add Serivce and Pipeline Details to the JSON
+  const getServicePipelineDetails = (data: any) =>{
+      console.log("Fourth Call - getting Service Pipeline Details");
+
+    const pipelineName = props.pipeline.name;
+    const index = data.services.findIndex((service:any) => service.serviceName == pipelineName);
+    if(index >= 0){
+      props.stage.serviceId = data.services[index].serviceId;
+      const pipelines =  data.services[index].pipelines;
+      const pipelineIndex = pipelines.findIndex((pipeline:any) => pipeline.pipelineName == pipelineName);
+      if(pipelineIndex >= 0){
+        props.stage.pipelineId = pipelines[pipelineIndex].pipelineId;
+      }
+    }
+  }
+
+
+
 
   
   // const { result: environmentsList} = useData(
@@ -69,10 +175,15 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
   // );
 
   const handleOnEnvironmentSelect = (e:any, props:any) => {
+      console.log("Loaded handleOnEnvironmentSelect", props);
+
     const index = e.target.value;
     const spinnValue = environmentsList.filter(e => e.id == index)[0].spinnakerEnvironment;
           props.formik.setFieldValue("parameters.environment[0]['id']", index);
-          props.formik.setFieldValue("parameters.environment[0]['spinnakerEnvironment']", spinnValue);
+          props.formik.setFieldValue("parameters.environment[0]['spinnakerEnvironment']", spinnValue);;
+
+                console.log("After update", props);
+
   }
 
 
@@ -84,13 +195,33 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
     get()
     .then(
       (results)=> {
-        setenvironmentsList(results);       
+        setpolicyList(results);       
       }     
     )
   }, []) 
   // const { result: policyList} = useData(
   //   () => IgorService.getPolicyList(), [], []
   // )
+
+
+  // Approval Groups
+  const [approvalGroupsList , setapprovalGroupsList] = useState([]);
+  useEffect(()=> {  
+    REST('platformservice/v2/usergroups').
+    get()
+    .then(
+      (results)=> {
+        setapprovalGroupsList(results);       
+      }     
+    )
+  }, []) 
+
+  const handleApprovalGroups = (e:any, props:any) => {
+    console.log("handle Approval Groups: ", props);
+    console.log("e Value: ", e);
+    
+    props.formik.setFieldValue(`parameters.approvalGroups`, e)
+  }
   
   // const [checked, setChecked] = useState(false);
 
@@ -101,6 +232,11 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
   }; 
 
   const singleFieldComponent = (fieldParams: any) => {
+
+    fieldParams = props.stage.parameters ?? null
+    if(!fieldParams && (fieldParams.connectors)){
+      return;
+    }
     return fieldParams.connectors.map((dynamicField: any, index: number) => {
       if (dynamicField.supportedParams.length < 2 && !dynamicField.isMultiSupported) {
         HelpContentsRegistry.register(
@@ -125,7 +261,13 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
 
   const [chosenStage] = React.useState({} as IStageForSpelPreview);
 
-  const multiFieldComp = (props: any, fieldParams: any) => {
+  const multiFieldComp = (props: any) => {
+    console.log("Loaded multiFieldComp", props);
+
+    const fieldParams = props.formik.values.parameters ?? null
+    if(!(fieldParams && fieldParams.connectors)){
+      return;
+    }
     return fieldParams.connectors.map((dynamicField: any, index: number) => {
       if (
         (dynamicField.supportedParams.length > 0 && dynamicField.isMultiSupported) ||
@@ -162,18 +304,28 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
     });
   };
 
-  const renderComponent = (props: any) => {
-    let renderContent = null;
-    const fieldParams = props.formik.values.parameters ?? null;
-    if (fieldParams !== null) {
-      renderContent = (
-        <>
+  const renderComponent = (props: IFormikStageConfigInjectedProps) => {
+              console.log("Third Call");
+              const propsFormik = props.formik.values;
 
+              if(!propsFormik?.parameters){
+                console.log("Props parameters not present", props);
+                
+                return;
+              }
+                console.log("Props present ---", props);
 
-            <div className="grid-span-1">
+    // let renderContent = null;
+    // const fieldParams = propsFormik?.parameters ?? null;
+    // if (fieldParams !== null) {
+      // renderContent = (
+        return <>
+
+            <div className="grid-span-2 fullWidthContainer">
+
                <FormikFormField
-                // name={props.formik.values.parameters.environment[0].spinnakerEnvironment}
-                name="parameters.environment[0].id"
+                // name={props.stage.parameters.environment[0].spinnakerEnvironment}
+                name="parameters.environment[0]"
                 label="Environment"
                 input={() => (
                   <ReactSelectInput
@@ -191,7 +343,7 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
                         label: item.spinnakerEnvironment
                       }))}
                   //value={...props}
-                  value={`${props.formik.values.parameters.environment[0].id}`}
+                  value={propsFormik?.parameters.environment[0].id}
                   // stringOptions={...props}
                   />
                 )}
@@ -201,11 +353,11 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
 
             {/* Automated Approval */}
 
-            <div className="grid-span-1">
-              <input type="checkbox" checked={props.formik.values?.parameters?.isAutomatedApproval} onChange={(e) => handleCheckbox(e, props)}  />
+            <div className="grid-span-2 fullWidthContainer">
+              <input type="checkbox" checked={propsFormik?.parameters?.isAutomatedApproval} onChange={(e) => handleCheckbox(e, props)}  /> Automated Approval
 
-               {props.formik.values?.parameters?.isAutomatedApproval ? 
-              <div className="grid grid-4 form mainform">
+               {propsFormik?.parameters?.isAutomatedApproval ? 
+              <div className="grid grid-4">
                 
                 <div className="grid-span-1">              
                   <FormikFormField
@@ -234,42 +386,56 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
 
             </div>
 
-          <div className="grid-span-4 fullWidthContainer">
+
+            <div className="grid-span-1">
+              <p>Approval Groups</p> 
+              <MultiSelect
+                options = {approvalGroupsList && approvalGroupsList.map((approvalGroup: any) => ({
+                            label: approvalGroup.userGroupName,
+                            value: approvalGroup.userGroupId
+                          }))} 
+                onChange={(e:any) => handleApprovalGroups(e,props)}
+                labelledBy="Select"
+                value={propsFormik?.parameters?.approvalGroups}
+              /> 
+            </div>
+
+          {/* <div className="grid-span-4 fullWidthContainer">
             <FormikFormField
               name="parameters.gateUrl"
               label="Gate Url"
               help={<HelpField id="opsmx.approval.gateUrl" />}
               input={(props) => <TextInput className="fullWidthField" {...props} />}
             />
-          </div>
-          {singleFieldComponent(fieldParams)}
-          {multiFieldComp({ ...props }, fieldParams)}
+          </div> */}
+          {/* {singleFieldComponent(fieldParams)}*/}
+          {multiFieldComp({ ...props })} 
 
-          <div className="grid-span-4 fullWidthContainer">
+          {/* <div className="grid-span-4 fullWidthContainer">
             <FormikFormField
               name="parameters.imageIds"
               label="Image Ids"
               help={<HelpField id="opsmx.approval.imageIds" />}
               input={(props) => <TextInput className="fullWidthField" {...props} />}
             />
-          </div>
+          </div> */}
 
           <div className='p-4'>
               <div className="grid-span-4">
-                <h4 className="sticky-header ng-binding">Gate Security</h4>
+                <h4>Gate Security</h4>
                 <br />
                 <div className="grid-span-2">
-                  {fieldParams.gateUrl}
+                  {/* {propsFormik.gateUrl} */}
                 </div>
-                {multiFieldGateSecurityComp({ ...props }, fieldParams)}
+                {multiFieldGateSecurityComp({ ...props })}
               </div>
         </div>
         </>
-      );
-    } else {
-      renderContent = exceptionDiv;
-    }
-    return renderContent;
+      // );
+    // } else {
+    //   renderContent = exceptionDiv;
+    // }
+    // return renderContent;
   };
 
 
@@ -277,8 +443,16 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
 
   // const fieldParams = { "connectors": [{ "connectorType": "PayloadConstraints", "helpText": "Payload Constraints", "isMultiSupported": true, "label": "Payload Constraints", "supportedParams": [{ "helpText": "Key", "label": "Key", "name": "label", "type": "string" }, { "helpText": "Value", "label": "Value", "name": "value", "type": "string" }], "values": [{ "label": "${bn}", "value": "Dev-run-tests-on-staging-master" }] }], "gateUrl": "https://isd.prod.opsmx.net/gate/visibilityservice/v5/approvalGates/3/trigger", "imageIds": "opsmx:latest" }
   
-  const multiFieldGateSecurityComp = (props: any, fieldParams:any) => {
-    // const fieldParams = props.formik.values.parameters ?? null
+  const multiFieldGateSecurityComp = (props: any) => {
+    getGateSecurityParams();
+              console.log("Loaded multiFieldGateSecurityComp", props);
+
+
+    const fieldParams = props.formik.values?.parameters ?? null
+    if(!(fieldParams && fieldParams.gateSecurity)){
+      return;
+    }
+    // const fieldParams = props.stage.parameters ?? null
     return fieldParams.gateSecurity.map((dynamicField: any, index: number) => {
       if (
         (dynamicField.supportedParams.length > 0 && dynamicField.isMultiSupported) ||
@@ -327,7 +501,8 @@ export function VisibilityApprovalConfig(props: IStageConfigProps) {
           <div>
           <div className="flex">
             <div className="grid leftGrid"></div>
-            <div className="grid grid-4 mainForm">{renderComponent({ ...props })}</div>
+            {/* <div className="grid grid-4 mainForm">{renderComponent({ ...props })}</div> */}
+            <div className="grid grid-4">{renderComponent({ ...props })}</div>
             <div className="opsmxLogo">
               <img
                 src="https://cd.foundation/wp-content/uploads/sites/78/2020/05/opsmx-logo-march2019.png"
