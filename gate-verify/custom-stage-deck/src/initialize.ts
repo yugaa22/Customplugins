7f01c00e619b2cc146d60b8fb665ffb1c0fcc73f
Sahiti Tarigoppula <sahiti.tarigoppula@opsmx.io>
2022-07-08 19:09:12 +0530

    op-16221-Hide starttime fields on realtime checked

--- gate-verify/custom-stage-deck/src/initialize.ts
+++ gate-verify/custom-stage-deck/src/initialize.ts
@@ -19,15 +19,15 @@ export const initialize = () => {
   HelpContentsRegistry.register('opsmx.verification.logAnalysis', 'Please Enable Log Analysis with true / false');
   HelpContentsRegistry.register('opsmx.verification.metricAnalysis', 'Please Enable Metric Analysis with true / false');
   HelpContentsRegistry.register('opsmx.verification.baselineStartTime', 'Baseline Start Time for the analysis');
-  HelpContentsRegistry.register('opsmx.verification.canaryStartTime', 'Canary Start Time for the analysis');
+  HelpContentsRegistry.register('opsmx.verification.canarystarttime', 'Canary Start Time for the analysis');
   HelpContentsRegistry.register('opsmx.verification.gateName', 'Please enter Gate Name that is created in OES');
   //HelpContentsRegistry.register('opsmx.verification.imageIds', 'Unique Id which represents a pipeline execution; For example, in a k8s deployment pipeline, it is the Image Id');
   HelpContentsRegistry.register('opsmx.verification.logTemplate', 'A collection of all the information needed to run the log analysis');
   HelpContentsRegistry.register('opsmx.verification.metricTemplate', 'Information needed to run the metric analysis');
   HelpContentsRegistry.register('opsmx.verification.environment', 'Environment in which the Analysis will run');  
   HelpContentsRegistry.register('opsmx.verification.customEnvironment', 'Add New Environment');
-  HelpContentsRegistry.register('opsmx.verification.canaryRealTime', 'Canary RealTime');
-  HelpContentsRegistry.register('opsmx.verification.baselineRealTime', 'Baseline RealTime');  
+  HelpContentsRegistry.register('opsmx.verification.canaryRealTime', 'Start time of stage execution will be taken as Canary Start time');
+  HelpContentsRegistry.register('opsmx.verification.baselineRealTime', 'Start time of stage execution will be taken as Baseline Start time');  
   HelpContentsRegistry.register('opsmx.verification.marginalScore', 'If the score is equal to or higher than the Marginal but lower than the Pass Score, “Review” status is shown in the Analysis screen'); 
   HelpContentsRegistry.register('opsmx.verification.passScore', 'If the Score equal to or higher than Pass Score, the stage will Pass');
 };
