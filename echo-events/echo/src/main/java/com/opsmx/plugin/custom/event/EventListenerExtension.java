f2dcdf3a6588af39dda94ea295284189cfd15ab5
Sri Harsha Kancharla <harsha.kancharla@opsmx.io>
2023-07-05 11:21:56 +0530

    OP-20383 : Added spinnaker name in the event (#184)
    * OP-20383 : Bug fix for pushing multiple event to rabbitmq
    
    * OP-20383 : Bug fix for pushing multiple event to rabbitmq
    
    * OP-20383 : Bug fix for pushing multiple event to rabbitmq
    
    * OP-20383 : Added spinnaker name in the event

--- echo-events/echo/src/main/java/com/opsmx/plugin/custom/event/EventListenerExtension.java
+++ echo-events/echo/src/main/java/com/opsmx/plugin/custom/event/EventListenerExtension.java
@@ -8,6 +8,7 @@ import com.netflix.spinnaker.echo.api.events.Event;
 import com.netflix.spinnaker.echo.api.events.EventListener;
 import com.netflix.spinnaker.kork.plugins.api.spring.ExposeToApp;
 import com.opsmx.plugin.custom.event.config.CamelConfig;
+import com.opsmx.plugin.custom.event.config.SpinnakerConfig;
 import com.opsmx.plugin.custom.event.config.SsdConfig;
 import com.opsmx.plugin.custom.event.constants.EchoConstant;
 import org.apache.camel.CamelContext;
@@ -19,6 +20,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
 import java.util.*;
 
 @Primary
@@ -41,6 +44,9 @@ public class EventListenerExtension implements EventListener {
     @Autowired
     private SsdConfig ssdConfig;
 
+    @Autowired
+    private SpinnakerConfig spinnakerConfig;
+
     @Override
     public void processEvent(Event event) {
         try {
@@ -53,8 +59,10 @@ public class EventListenerExtension implements EventListener {
                 String message = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(eventMap);
                 producerTemplate.sendBody(EchoConstant.echoEventDirectEndPointUrl, message);
 
-                if (ssdConfig.isEnable())
+                if (ssdConfig.isEnable()) {
+                    eventMap.put("spinnakerName", spinnakerConfig.getName());
                     ssdEvents(eventMap);
+                }
             }
         } catch (Exception e) {
             logger.error("Exception occurred while processing event : {}", e);
@@ -74,13 +82,16 @@ public class EventListenerExtension implements EventListener {
                 LinkedHashMap execution = (LinkedHashMap) content.get("execution");
                 if (execution.containsKey("stages") && execution.get("stages") != null) {
                     ArrayList stages = (ArrayList) execution.get("stages");
+                    String ssdMessage = "";
                     for (Object stage : stages) {
                         Map<String, Object> stageMap = mapper.convertValue(stage, Map.class);
                         if (pipelineStatus && stageMap.containsKey("type") && stageMap.get("type").toString().trim().equals("deployManifest")
                                 && stageMap.containsKey("status") && stageMap.get("status").toString().trim().equals("SUCCEEDED")) {
-                            String ssdMessage = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(eventMap);
-                            producerTemplate.sendBody(EchoConstant.echoEventDirectEndPointUrlForSSD, ssdMessage);
+                            ssdMessage = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(eventMap);
                         }
+                    }
+                    if (!StringUtils.isEmpty(ssdMessage)) {
+                        producerTemplate.sendBody(EchoConstant.echoEventDirectEndPointUrlForSSD, ssdMessage);
                     }
                 }
             }
